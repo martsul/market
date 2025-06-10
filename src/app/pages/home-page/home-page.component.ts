7ad589e3575c859feddb00f93c7c2876d91fe53b
Aleksandr Martsul <a.martsul@itransition.com>
2025-06-10 17:36:52 +0300

    brands, brands mobile

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HomePreviewComponent } from '../../modules/home-preview/home-preview.component';
+import { BrandsComponent } from '../../modules/brands/brands.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HomePreviewComponent],
+  imports: [HomePreviewComponent, BrandsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
